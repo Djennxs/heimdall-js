abaf5805f6fdae4220914fba386fd03aff43dc54
Owndragoon <kent_anders92@hotmail.com>
2021-03-23 15:45:06 +0100

    Testing DM prospect
    Testing DM prospect; Not functional.

---
+++ commands/faq.js
@@ -0,0 +1,34 @@
+const config = require('../config.json');
+const { MessageEmbed } = require('discord.js');
+
+const helpCommand = (Client) => {
+    Client.on('ready', () => {
+        console.log('Help command ready!');
+    });
+}
+
+Client.on('message', message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    const channel = await getUserFromMention(Client, args[0]).createDM();
+    const user = await channel.send('Hello there! Here is a FAQ for Viking PMC, to guide you through the installation, common questions, or similar.');
+}
+
+/**
+* 
+* Helper functions
+*/
+const getUserFromMention = (Client, mention) => {
+  if (!mention) return;
+  
+  if (mention.startsWith('<@') && mention.endsWith('>')) {
+    mention = mention.slice(2, -1);
+    
+    if (mention.startsWith('!')) {
+      mention = mention.slice(1);
+    }
+    
+    return Client.users.cache.get(mention);
+  }
+}
+
+module.exports = { faqCommand };
