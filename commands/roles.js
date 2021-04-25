32094d1f678be71eda4e53ab693f6dbed8e7ea2a
Dennis van der Waag <dennisvanderwaag@gmail.com>
2021-04-25 22:04:51 +0200

    things

--- commands/roles.js
+++ commands/roles.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const { handleRoles } = require('../Functions/roles');
 const config = require('../config.json');
 
@@ -33,7 +34,34 @@ const roleCommands = (Client) => {
       });
 
       // Remove the command message
-      message.delete();
+      message.delete().catch(error => {
+        message.guild.members.fetch(`${config.logs.user}`).then(user => {
+          const date = new Date(message.createdTimestamp).toLocaleString("nl-NL");
+          const embed = new MessageEmbed()
+          .setColor('0xff0000')
+          .setTitle('Shit, fuck! Something went wrong!')
+          .setDescription('You recieved this message because someone fucked up, which is probably us.\n\u200B');
+
+          console.log(date);
+
+          embed.addFields(
+            {
+              name: `Date and time:`,
+              value: `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}\n\u200B`
+            },
+            {
+              name: `Location:`,
+              value: `<#${message.channel.id}>\n\u200B`
+            },
+            {
+              name: `Error:`,
+              value: `\`\`\`${error}\`\`\``
+            }
+          );
+
+          user.send(embed);
+        });
+      });
     }
   });
 }
